d6fcdae134a64d4b8635a9d72a7683273401b0d1
Fandy Ni <fn1803@gmail.com>
2024-11-14 20:54:47 +0100

    testtest

--- task1.py
+++ task1.py
@@ -1,23 +1,3 @@
-from llama_cpp import Llama
-model_path = "models/llama-2-7b.Q4_K_M.gguf"
-
-# If you want to use larger models...
-#model_path = "models/llama-2-13b.Q4_K_M.gguf"
-# All models are available at https://huggingface.co/TheBloke. Make sure you download the ones in the GGUF format
-#The goal of the assignment is to implement a program that post-processes the output of large language models (like ChatGPT, etc.) to improve the quality of its answers.
-question = "What is the capital of The Netherlands? "
-llm = Llama(model_path=model_path, verbose=False)
-print("Asking the question \"%s\" to %s (wait, it can take some time...)" % (question, model_path))
-output = llm(
-      question, # Prompt
-      max_tokens=16, # Generate up to 32 tokens
-      stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
-      echo=True # Echo the prompt back in the output
-)
-print("Here is the output")
-print(output['choices'])
-
-
 
 import re
 
@@ -34,23 +14,26 @@ def clean_text(text):
     text = re.sub(r'http\S+|www\S+', '', text)  # Remove URLs
     text = re.sub(r'[^a-zA-Z\s]', '', text)  # Remove special characters
     text = re.sub(r'\s+', ' ', text).strip()  # Remove extra whitespace
+    text = re.sub(r'<[^>]+>', '', text)  # HTML tags
+    text = re.sub(r'\*\*(.*?)\*\*', r'\1', text)  # Markdown bold
+    text = re.sub(r'\d+', '', text)
+    ##here i remove some common stuff
+    redundant_phrases = [
+        "please note",
+        "kind regards",
+        "for more information",
+        "as mentioned above",
+        "thank you for your time"
+    ]
+    for phrase in redundant_phrases:
+        text = text.replace(phrase, '')
+
+    from nltk.corpus import stopwords
+    stop_words = set(stopwords.words('english'))
+    text = ' '.join([word for word in text.split() if word not in stop_words])
+
+
     return text
 
-# Clean the input question
-cleaned_question = clean_text(question)
-print("Cleaned Question:", cleaned_question)
 
 
-
-# Query the Llama model
-print(f"Asking the question: '{cleaned_question}' (wait, it can take some time...)")
-output = llm(
-    cleaned_question,
-    max_tokens=16,
-    stop=["Q:", "\n"],
-    echo=True
-)
-
-# Process and print model output
-raw_output = output['choices'][0]['text']
-print("Raw output:", raw_output)
