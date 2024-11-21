eccad3cf7af9fcac13769f66359ea127273a6ede
Joseph Kanichai <kjosephsubash@gmail.com>
2024-11-21 16:59:39 +0100

    Setup main

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from preprocess import preprocess_text
-from remove_stopwords_and_normal import *
-#from fandy_clean_text import clean_text
+import argparse
+from llama_cpp import Llama
+from timeit import default_timer as timer
+model_path = "models/llama-2-7b.Q4_K_M.gguf"
 
-# example
-text = r"What is the capital of Italy"
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str, help="Model path", required=False)
+parser.add_argument("--gpu", action="store_true", help="Run with nvidia GPU", required=False)
+parser.add_argument("--input", type=str, help="Input question", required=True)
+parser.add_argument("--debug", action="store_true", help="Debug mode", required=False)
 
-print("preprocess text: ", preprocess_text(text))
+args = parser.parse_args()
 
-print(type(preprocess_text(text)))
+if args.model is not None and len(args.model) > 0:
+    model_path = args.model
+
+start = timer()
+llm = Llama(model_path=model_path, verbose=False, n_gpu_layers=-1 if args.gpu else 0)
+output = llm(args.input, max_tokens=32, stop=["Q:", "Question:", "Context:"], echo=False)
+end = timer()
+
+print(output['choices'][0]['text'])
+if args.debug:
+    print(f"Time: {end - start}")
